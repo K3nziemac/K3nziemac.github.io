9a4000340f7987de078720e4d432c37fc3b0b1d3
K3nziemac <duk14002@byui.edu>
2020-03-03 17:36:25 -0700

    zac

--- Locations/javascript.js
+++ Locations/javascript.js
@@ -1,9 +1,8 @@
-function onClickMenu(){
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
-	
-	document.getElementById("menu-bg").classList.toggle("change-bg");
+function onClickMenu() {
+    document.getElementById('menu').classList.toggle('change');
+    document.getElementById('nav').classList.toggle('change');
+    document.getElementById('menu-bg').classList.toggle('change-bg');
 }
-const todaysdate = new Date(); 
+const todaysdate = new Date();
 const options = {weekdays: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US' , options);
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
